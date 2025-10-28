0173d167c6f7c9e0af82a0d06295075a494d5547
Swar Shinde <162664467+Swarspage@users.noreply.github.com>
2025-10-28 12:19:22 +0530

    admission

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
 require("dotenv").config();
 
 // const createDefaultAdmin = require('./seedAdmin');
@@ -10,19 +10,25 @@ const PORT = 5000;
 
 const app = express();
 
-connectDB()
+connectDB();
 
 // Middlewares
-app.use(cors());
-app.use(express.json({limit: "50mb",}));
-app.use(express.urlencoded({limit: "50mb", extended: true}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 
 // createDefaultAdmin();
 
 // Routes
-app.use("/api/auth", require('./routes/authRoutes'));
-app.use("/api/internship" , require("./routes/internRoutes"));
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/internship", require("./routes/internRoutes"));
 app.use("/api/achievements", require("./routes/achievementRoutes"));
 app.use("/api/activities", require("./routes/activitiesRoutes"));
 app.use("/api/semesterInfo", require("./routes/semInfoRoutes"));
@@ -31,11 +37,9 @@ app.use("/api/placement", require("./routes/PlacementRoutes"));
 app.use("/api/higherStudies", require("./routes/HigherStudiesRoutes"));
 app.use("/api/student", require("./routes/StudentRoutes"));
 
-
-app.get("/" , (req, res) => {
+app.get("/", (req, res) => {
   res.send("API is running...");
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
