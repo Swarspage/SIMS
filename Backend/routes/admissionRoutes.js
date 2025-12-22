d2127aca54f2cd832592292e3694b7089e3e291b
sanikaSalunkhe103 <salunkhesakshi540@gmail.com>
2025-12-22 04:51:22 -0800

    done changes in admission routes by applying div incharge role

--- Backend/routes/admissionRoutes.js
+++ Backend/routes/admissionRoutes.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const verifyToken = require("../middlewares/VerifyToken");
+// const verifyToken = require("../middlewares/VerifyToken");
+
+const authenticateToken = require("../middlewares/authenticateToken");
+const authorizeRoles = require("../middlewares/authorizeRoles");
+const trimRequestBodyStrings = require("../middlewares/trimRequestBodyStrings");
+
 const {
   createAdmission,
   getAdmissionsByStudent,
@@ -11,17 +16,15 @@ const {
   getUnpaidStudents,
 } = require("../controllers/admissionController");
 
-// ==================== STUDENT ROUTES ====================
-router.post("/", verifyToken, createAdmission);
-router.get("/my-admissions", verifyToken, getAdmissionsByStudent);
-router.put("/:id", verifyToken, updateAdmission);
-router.delete("/:id", verifyToken, deleteAdmission);
+//routes
+router.post("/", authenticateToken, authorizeRoles("admin", "student", "divisionIncharge"), trimRequestBodyStrings, createAdmission);
+router.get("/my-admissions", authenticateToken, authorizeRoles("student"), trimRequestBodyStrings , getAdmissionsByStudent);
+router.put("/:id", authenticateToken, authorizeRoles("admin", "student", "divisionIncharge"), trimRequestBodyStrings, updateAdmission);
+router.delete("/:id", authenticateToken, authorizeRoles("admin", "student", "divisionIncharge"), trimRequestBodyStrings, deleteAdmission);
 
-// ==================== ADMIN ROUTES (ORDER MATTERS!) ====================
-// ⚠️ IMPORTANT: Specific routes MUST come before dynamic routes (/:id)
-
-router.get("/all", verifyToken, getAllAdmissions); // ✅ BEFORE /:id
-router.put("/status/:id", verifyToken, updateAdmissionStatus); // Specific
-router.get("/unpaid", verifyToken, getUnpaidStudents); // Specific
+//admin & DI Routes
+router.get("/all", authenticateToken, authorizeRoles("admin", "divisionIncharge"), trimRequestBodyStrings, getAllAdmissions); //BEFORE /:id
+router.put("/status/:id",  authenticateToken, authorizeRoles("admin", "divisionIncharge"), trimRequestBodyStrings, updateAdmissionStatus); // Specific
+router.get("/unpaid",  authenticateToken, authorizeRoles("admin", "divisionIncharge"), trimRequestBodyStrings, getUnpaidStudents); // Specific
 
 module.exports = router;
