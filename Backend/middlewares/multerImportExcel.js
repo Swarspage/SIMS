285f4be18066adbfa45cda5bae10006cdee214bf
AasthaOswal <aasthaoswal29@gmail.com>
2025-12-28 13:11:52 +0530

    Removed xlsx package as it had security vulnerability, and made mukter middlewares more suitable for deployment

--- Backend/middlewares/multerImportExcel.js
+++ Backend/middlewares/multerImportExcel.js
@@ -1,10 +1,15 @@
 //this middleware uses memoryStorage for import excel routes, where excelFile is being uploaded to server
 
-
 const multer = require("multer");
 
 const storage = multer.memoryStorage(); // everything is kept in memory
-const upload = multer({ storage });
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5 MB
+  },
+});
+
 
 const uploadMemoryStorage=upload;
 
